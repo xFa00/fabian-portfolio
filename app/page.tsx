0010fe0d6441295759803a2995d5e59b92ef9b3a
Fabián Chiran <111092524+xFa00@users.noreply.github.com>
2026-07-28 13:43:33 -0500

    feat: add skill section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Experience />
       <Projects />
+      <Skills />
     </main>
   );
 }
